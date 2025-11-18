9be428da65b19a0af98d646b2998aebc4ef80270
hereteksupport <cristian.d.gradinaru@gmail.com>
2025-11-18 12:33:55 -0700

    Refactored into multiple files. Minor fixes. Webpackify next

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// API Configuration and Constants
+export const API_KEY = 'e4b022b3fd43127c1081cf62bc74329c'; //Don't do this in production or I WILL weep uncontrollably
+export const API_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
+export const POSTER_SIZE = 'w500';
